95785df1730bd3292427063f185af2ded44c4bdf
Kiruthigadurairaj <146357922+Kiruthigadurairaj@users.noreply.github.com>
2025-01-22 18:37:45 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,10 @@
 //your JS code here. If required.
+const timerElement = document.getElementById("timer");
+
+setInterval(function () {
+    // Get the current date and time
+    const now = new Date();
+
+    // Update the timer element's content
+    timerElement.innerHTML = now.toLocaleString();
+}, 1000);
